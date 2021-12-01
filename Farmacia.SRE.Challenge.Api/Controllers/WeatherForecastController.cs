57b22523615e0915d9c5cd321f2c94a934e9e2ca
cflamenco98 <carlos.flamenco@farsiman.com>
2021-12-01 11:13:35 -0600

    config

--- Farmacia.SRE.Challenge.Api/Controllers/WeatherForecastController.cs
+++ Farmacia.SRE.Challenge.Api/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Farmacia.SRE.Challenge.Api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Flamenco", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
